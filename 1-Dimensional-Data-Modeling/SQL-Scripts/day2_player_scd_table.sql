e6b8fa6a995970fc4c1cb11bff092fa77256f57b
AdityaKalmegh <aditya.kalmegh@gmail.com>
2025-02-08 15:28:50 -0800

    Update day2_player_scd_table.sql

--- 1-Dimensional-Data-Modeling/SQL-Scripts/day2_player_scd_table.sql
+++ 1-Dimensional-Data-Modeling/SQL-Scripts/day2_player_scd_table.sql
@@ -1,9 +1,9 @@
-create table players_scd_table (
+create table players_scd(
 	player_name text,
 	scoring_class scoring_class,
 	is_active boolean,
 	start_season integer,
 	end_season integer,
 	current_season INTEGER,
-	PRIMARY KEY (player_name, current_season)
+	PRIMARY KEY (player_name, start_season)
 	);
